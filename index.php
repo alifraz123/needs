a9e39f2599b26f073e13a8ecba2cd58400751bf2
umar habib <36890130+alifraz123@users.noreply.github.com>
2022-02-07 16:09:02 +0500

    changes

--- index.php
+++ index.php
@@ -3,34 +3,27 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
-.loader {
-  border: 16px solid #f3f3f3;
-  border-radius: 50%;
-  border-top: 16px solid #3498db;
-  width: 120px;
-  height: 120px;
-  -webkit-animation: spin 2s linear infinite; /* Safari */
-  animation: spin 2s linear infinite;
-}
-
-/* Safari */
-@-webkit-keyframes spin {
-  0% { -webkit-transform: rotate(0deg); }
-  100% { -webkit-transform: rotate(360deg); }
-}
-
-@keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
-}
+ .loader {
+      position: fixed;
+      left: 50%;
+      top: 30%;
+      width: 10%;
+      height: 10%;
+      z-index: 9999;
+      background: url(loader.svg) 50% 50% no-repeat;
+    }
 </style>
 </head>
 <body>
 <div class="loader"></div>
-<?php
-header("Location: https://www.eneeds360.com/");
-
-?>
+<script>
+  window.onload = function(){
+    setTimeout(redirectFunction,3000);
+  }
+  function redirectFunction(){
+    location.href = 'https://www.eneeds360.com/'; 
+  }
+</script>
 
 </body>
 </html>
